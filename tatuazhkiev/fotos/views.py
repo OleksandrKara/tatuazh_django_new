a8acf9480f27f84c8a92f3c1dc8aa81a9ad5bd3d
Oleksandr <olexandr.kara@gmail.com>
2014-09-20 23:09:33 +0300

    Django was implemented

---
+++ tatuazhkiev/fotos/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+# Create your views here.
+
+def xhr_test(request):
+	name = request.POST.get('name')
+	phone = request.POST.get('phone')
+	send_mail(
+		"Please call me back!",
+        "Name: "+ name + "\nPhone: " + phone,
+		request.POST.get('olexandr.kara@gmail.com'),
+                ['kiev.tatuazh@gmail.com'],
+	)
+	
+	message = True
+	return HttpResponse(message)
+
+def kontakti_handler(request):
+    errors = []
+    if request.method == 'POST':
+        if not request.POST.get('subject', ''):
+            errors.append('Enter a subject.')
+        if not request.POST.get('message', ''):
+            errors.append('Enter a message.')
+        if not request.POST.get('phone', ''):
+            errors.append('Please enter phone.')
+        if not errors:
+            send_mail(
+                "Please read the letter and call me back!",
+                "Name:\t "+ request.POST['subject']+"\nPhone:\t "+request.POST['phone']+"\nMessage: "+request.POST['message'],
+                request.POST.get('olexandr.kara@gmail.com'),
+                ['kiev.tatuazh@gmail.com'],
+            )
+            return HttpResponseRedirect('/thanks/')
+    return render_to_response('kontakti.html/', {'errors': errors})
+
+def thanks(request):
+    return render_to_response('thanks.html')
---
+++ tatuazhkiev/views.py
@@ -0,0 +1,28 @@
+﻿# -*- coding: utf-8 -*-
+#from django.http import HttpResponse
+#from django.template.loader import get_template
+#from django.template import Context
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from fotos.models import Foto
+#from django.core.mail import send_mail
+#from django.http import HttpResponseRedirect
+
+def home_handler(request):
+    return render_to_response('home.html')
+
+def main_page(request):
+    return render_to_response('index.html')
+
+def ceni_handler(request):
+    return render_to_response('ceni.html')
+	
+def otzivi_handler(request):
+    return render_to_response('otzivi.html')
+
+def faq_handler(request):
+    return render_to_response('faq.html')
+
+def photo_handler(request):
+    fotos = Foto.objects.filter(type="Br")
+    return render_to_response('flatpages/photo.html', {'fotos' : fotos}, context_instance = RequestContext(request))
